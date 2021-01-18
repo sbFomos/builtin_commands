5b8b1ecf3c73c034f69b24a89bcbf1201206c372
NathanMcMillan54 <nathanmcmillan54@gmail.com>
2021-01-17 19:23:26 -0500

    Added print executable for armv6, armv7, and x86/x86_64.

--- print/src/lib.rs
+++ print/src/lib.rs
@@ -9,16 +9,15 @@ const L3: &str = "Never gonna run around and desert you\n\0";
 const L4: &str = "Never gonna make you cry\n\0";
 const L5: &str = "Never gonna say goodbye\n\0";
 const L6: &str = "Never gonna tell a lie and hurt you\n\0";
-const RICK: &str = "\n__rick_astley__\n\0";
 
-unsafe fn easter_egg() {
+#[no_mangle]
+pub unsafe extern "C" fn easter_egg() {
     printf(L1.as_ptr() as *const _);
     printf(L2.as_ptr() as *const _);
     printf(L3.as_ptr() as *const _);
     printf(L4.as_ptr() as *const _);
     printf(L5.as_ptr() as *const _);
     printf(L6.as_ptr() as *const _);
-    printf(RICK.as_ptr() as *const _);
 }
 
 #[cfg(any(target_arch = "arm", target_arch = "aarch64"))]
@@ -31,10 +30,6 @@ extern "C" { fn printf(format: *const c_char) -> i32; }
 
 #[no_mangle]
 pub extern "C" fn print(arg: &str) {
-    if arg == "__rick_astley__" {
-        unsafe { easter_egg(); }
-    } else {
-        unsafe { printf(arg.as_ptr() as *const _); }
-        unsafe { printf("\n\0".as_ptr() as *const _); }
-    }
+    unsafe { printf(arg.as_ptr() as *const _); }
+    unsafe { printf("\n\0".as_ptr() as *const _); }
 }
