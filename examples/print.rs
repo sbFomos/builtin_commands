0f8f5ac624d123481f642133ffb38a17a542e645
NathanMcMillan54 <nathanmcmillan54@gmail.com>
2021-01-18 17:40:01 -0500

    This library will be apart of FOMOSv2.

--- examples/print.rs
+++
@@ -1,7 +0,0 @@
-extern crate builtin_commands;
-use builtin_commands::print_main;
-
-fn main() {
-    print_main("Hello world");
-    print_main("__rick_astley__");
-}
--- print/src/lib.rs
+++ print/src/lib.rs
@@ -1,22 +1,5 @@
 #![no_std]
 
-const L1: &str = "Never gonna give you up\n\0";
-const L2: &str = "Never gonna let you down\n\0";
-const L3: &str = "Never gonna run around and desert you\n\0";
-const L4: &str = "Never gonna make you cry\n\0";
-const L5: &str = "Never gonna say goodbye\n\0";
-const L6: &str = "Never gonna tell a lie and hurt you\n\0";
-
-#[no_mangle]
-pub unsafe extern "C" fn easter_egg() {
-    printf(L1.as_ptr() as *const _);
-    printf(L2.as_ptr() as *const _);
-    printf(L3.as_ptr() as *const _);
-    printf(L4.as_ptr() as *const _);
-    printf(L5.as_ptr() as *const _);
-    printf(L6.as_ptr() as *const _);
-}
-
 #[cfg(any(target_arch = "arm", target_arch = "aarch64"))]
 type c_char = u8;
 
@@ -25,8 +8,7 @@ type c_char = i8;
 
 extern "C" { fn printf(format: *const c_char) -> i32; }
 
-#[no_mangle]
-pub extern "C" fn print_main(arg: &str) {
+pub extern fn print_main(arg: &str) {
     unsafe { printf(arg.as_ptr() as *const c_char); }
     unsafe { printf("\n".as_ptr() as *const c_char); }
 }
