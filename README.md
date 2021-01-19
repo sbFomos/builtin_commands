78b2636f19eefce2bb6aa780115a4efd10afe441
NathanMcMillan54 <nathanmcmillan54@gmail.com>
2021-01-18 22:21:24 -0500

    Seeing if using libc::printf works on FOMOS instead of call printf externally.

--- README.md
+++ README.md
@@ -1,4 +1,1 @@
-## About
-
-These are the builtin and most important commands for FOMOSv2-CL. Read ``Documentation/`` for examples of command and
-applications and how to write them.
+This branch is for testing new code for FOMOS commands before committing to the master branch.
--- print/src/lib.rs
+++ print/src/lib.rs
@@ -1,14 +1,15 @@
 #![no_std]
 
+extern crate libc;
+use libc::printf;
+
 #[cfg(any(target_arch = "arm", target_arch = "aarch64"))]
 type c_char = u8;
 
 #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
 type c_char = i8;
 
-extern "C" { fn printf(format: *const c_char) -> i32; }
 
 pub extern fn print_main(arg: &str) {
     unsafe { printf(arg.as_ptr() as *const c_char); }
-    unsafe { printf("\n".as_ptr() as *const c_char); }
 }
