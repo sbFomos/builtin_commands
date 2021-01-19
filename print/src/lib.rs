af0b1df7c89ff1c4324e52540fbf1d3239d86ed8
NathanMcMillan54 <nathanmcmillan54@gmail.com>
2021-01-19 12:03:00 -0500

    Print command uses libc crate instead of calling printf externally.

--- print/src/lib.rs
+++ print/src/lib.rs
@@ -1,14 +1,13 @@
 #![no_std]
 
-#[cfg(any(target_arch = "arm", target_arch = "aarch64"))]
-type c_char = u8;
+extern crate libc;
+use libc::{c_char, c_int, printf};
 
-#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
-type c_char = i8;
-
-extern "C" { fn printf(format: *const c_char) -> i32; }
+pub fn space() -> &'static str {
+    "\n\0"
+}
 
 pub extern fn print_main(arg: &str) {
     unsafe { printf(arg.as_ptr() as *const c_char); }
-    unsafe { printf("\n".as_ptr() as *const c_char); }
+    unsafe { printf(space().as_ptr() as *const c_char); }
 }
