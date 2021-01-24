1b2ec9a713528ab3007ba3bcc18ee7a590d1e417
NathanMcMillan54 <nathanmcmillan54@gmail.com>
2021-01-24 09:56:02 -0500

    asku command now works.

--- asku/src/password.c
+++ asku/src/password.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
+#include <string.h>
 
 int checkPassword() {
     FILE *fp;
-    char askuInput;
+    char askuInput[24];
     char passwordFile[24];
+    scanf("%s", askuInput);
     fp = fopen("/configs/user/password", "r");
     while (fgets(passwordFile, 24, fp) != NULL)
-    scanf("%s", askuInput);
-    if (askuInput = passwordFile) {
+    if (!strcmp(askuInput, passwordFile)) {
         return 1;
-    } return 0;
+    } else {
+        return 0;
+    }
 }
