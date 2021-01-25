7e97acd330e7d50295637c8975c2aba6d551ce47
NathanMcMillan54 <nathanmcmillan54@gmail.com>
2021-01-25 12:22:13 -0500

    I doubt this will make a diffrence, changed spacing between lines in print and shutdown command.

--- help/src/help.rs
+++ help/src/help.rs
@@ -1,2 +1,2 @@
 pub const HELP: &'static str = "The help command (this command) is used for showing a description
- or example of a command\nCommand Example:\n\n    help help\n\n\0";
+ or example of a command.\nCommand Example:\n\n    help help\n\n\0";
