2197980876f775c72f04d56d2db6a875859537ef
NathanMcMillan54 <nathanmcmillan54@gmail.com>
2021-01-26 07:34:35 -0500

    Attempt #7 for making help command work.

--- help/src/lib.rs
+++ help/src/lib.rs
@@ -15,15 +15,27 @@ unsafe fn all() {
 }
 
 pub fn help_main(arg: &str) {
+    let mut i = 0;
     if arg == "all" {
-        unsafe { all(); }
+        i = i + 0;
     } else if arg == "asku" {
-        unsafe { libc::printf(asku::ASKU.as_ptr() as *const libc::c_char); }
+        i = i + 2;
     } else if arg == "print" {
-        unsafe { libc::printf(print::PRINT.as_ptr() as *const libc::c_char); }
+        i = i + 1;
     } else if arg == "help" {
-        unsafe { libc::printf(help::HELP.as_ptr() as *const libc::c_char); }
+        i = i + 4;
     } else if arg == "shutdown" {
+        i = i + 3;
+    }
+    if i = 0 {
+        unsafe { all(); }
+    } else if i = 1 {
+        unsafe { libc::printf(print::PRINT.as_ptr() as *const libc::c_char); }
+    } else if i = 2 {
+        unsafe { libc::printf(asku::ASKU.as_ptr() as *const libc::c_char); }
+    } else if i = 3 {
         unsafe { libc::printf(shutdown::SHUTDOWN.as_ptr() as *const libc::c_char); }
+    } else if i = 4 {
+        unsafe { libc::printf(help::HELP.as_ptr() as *const libc::c_char); }
     }
 }
